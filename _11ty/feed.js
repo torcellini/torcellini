78940e69f534ac950e77495fdbc5acceceb187de
Micah Torcellini <micah@torcellini.org>
2023-01-06 10:42:33 -0500

    array sorting

--- _11ty/feed.js
+++ _11ty/feed.js
@@ -1,19 +1,28 @@
 let Parser = require('rss-parser');
 let parser = new Parser();
+const { DateTime } = require("luxon");
+const EleventyExtensionMap = require('@11ty/eleventy/src/EleventyExtensionMap');
 // use luxon to make the date look nice
-// generate a new array sorted by date before looping, so the posts are in the right order
-module.exports = async function(url) {
+function readableDate(d) {
+    return DateTime.fromISO(d, {zone: 'utc'}).
+    toFormat("LLLL dd, yyyy");
+};
+// compare the dates for sorting
+function compareDate(a, b) {
+    aDate = Date.parse(a.isoDate);
+    bDate = Date.parse(b.isoDate);
+    return bDate - aDate;
+};
+// TODO: build error handling
+module.exports = async function(url, number) {
     let feed = await parser.parseURL(url);
-    let html = '<h3>Latest</h3><ol>'
-    feed.items.forEach(function(entry) {
-        html = html + `<li><a href="${entry.link}">${entry.title}&nbsp;${entry.pubDate}</a>`;
+    let html = '<h3>Latest</h3><ol>';
+    let array = feed.items.sort(compareDate);
+    array.forEach(function(entry) {
+        html = html + `<li><a href="${entry.link}">${entry.title}&nbsp;${readableDate(entry.isoDate)}</a>`;
     })
     html = html + "</ol>"
     return html;
 }
-// fetch url
-    // parse to array of objects
-    // generate html list
-    // do this all async
 
 
