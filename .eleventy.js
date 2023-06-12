5881b05bab6d4ac6e0ac4aa58ef57d062de7fb42
Micah Torcellini <micah@torcellini.org>
2023-06-11 21:56:06 -0400

    beginning work on styling

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,11 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addAsyncShortcode("feed", require("./_11ty/feed.js"));
+    eleventyConfig.addPassthroughCopy("assets/js");
+    eleventyConfig.addPassthroughCopy("assets/css");
+    eleventyConfig.addPassthroughCopy("assets/files");
+    eleventyConfig.addPassthroughCopy("assets/fonts");
+    eleventyConfig.addPassthroughCopy("assets/images/site");
+    eleventyConfig.addPassthroughCopy("robots.txt");
+    eleventyConfig.addPassthroughCopy("CNAME");
+    eleventyConfig.addPassthroughCopy("favicon.ico");
 };
